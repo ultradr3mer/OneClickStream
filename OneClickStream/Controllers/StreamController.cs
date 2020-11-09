435509f9cf4fab207fc1ceb0224fa71623e6b2ef
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2020-11-09 08:38:27 +0100

    Initial checkin

---
+++ OneClickStream/Controllers/StreamController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneClickStream.Properties;
+
+namespace OneClickStream.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class StreamController : ControllerBase
+  {
+    #region Methods
+
+    [HttpGet]
+    public ContentResult Get(string source)
+    {
+      string html = Resources.AzureMediaPlayer;
+      html = html.Replace("{source}", source);
+      return base.Content(html, "text/html");
+    }
+
+    #endregion Methods
+  }
+}
